84ec70940915e1c67ef8476fc3a2d6b58ec739bf
CedricPradels <cedric.pradels@gmail.com>
2020-11-17 22:16:34 +0100

    Fix Heroku issue

--- src/utils/scrapping.ts
+++ src/utils/scrapping.ts
@@ -9,9 +9,12 @@ import { constants } from '../config/constants';
 
 export const openBrowser = async () => {
   const herokuArgs: LaunchOptions['args'] = ['--no-sandbox'];
+  const pmuArgs: LaunchOptions['args'] = [
+    '--proxy-server=http://10.10.10.10:8000',
+  ];
 
   const launchOptions: LaunchOptions = {
-    args: [...herokuArgs],
+    args: [...herokuArgs, ...pmuArgs],
   };
 
   const browser = await puppeteer.use(StealthPlugin()).launch(launchOptions);
