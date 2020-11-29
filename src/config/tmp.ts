b4eb6fa1208da2849c05476da098bfca8164b603
CedricPradels <cedric.pradels@gmail.com>
2020-11-29 10:16:33 +0100

    tmp

--- src/config/tmp.ts
+++ src/config/tmp.ts
@@ -5,7 +5,7 @@ export const tmp = async () => {
   const browser = await openBrowser();
   const page = await openPage(browser);
 
-  await page.setExtraHTTPHeaders({ connection: 'keep-alive' });
+  await page.setExtraHTTPHeaders({ 'X-Connection': 'keep-alive' });
   await page.goto(`${constants.serverURL}/test`, { waitUntil: 'networkidle0' });
 
   const urls = await getRacesURL('2020-11-29', page);
