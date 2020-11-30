5be32fec0a7fefd0171fab833a41a193a1b35547
CedricPradels <cedric.pradels@gmail.com>
2020-11-30 08:59:23 +0100

    tmp

--- src/config/tmp.ts
+++ src/config/tmp.ts
@@ -1,3 +1,4 @@
+import Axios from 'axios';
 import { constants } from '../config';
 import { openBrowser, openPage, getRacesURL } from '../utils/scrapping';
 
@@ -5,7 +6,12 @@ export const tmp = async () => {
   const browser = await openBrowser();
   const page = await openPage(browser);
 
+  await page.setExtraHTTPHeaders({ connection: 'keep-alive' });
   await page.goto(`${constants.serverURL}/test`, { waitUntil: 'networkidle0' });
+
+  Axios.get(`${constants.serverURL}/test`, {
+    headers: { connection: 'keep-alive' },
+  });
 
   const urls = await getRacesURL('2020-11-29', page);
   console.log(urls);
